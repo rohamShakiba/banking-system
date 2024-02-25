05cf9b453212e4a168710482f4580df522450f26
mrtzmhmmd <mrtz.mhmmd@yahoo.com>
2024-02-25 11:03:08 +0330

    Added Junit

---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import banking.entity.BankAccount;
+import exception.InsufficientFundsException;
+
+public class BankAccountTest {
+    private BankAccount account;
+
+    @Before
+    public void setUp() {
+        account = new BankAccount("123456789",
+                "Morteza Mohammadi",
+                1000.0);
+    }
+
+    @Test
+    public void testDeposit() {
+        account.deposit(500.0);
+        assertEquals(1500.0, account.getBalance(), 0.01);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDepositNegativeAmount() {
+        account.deposit(-500.0);
+    }
+
+    @Test
+    public void testWithdraw() {
+        account.withdraw(500.0);
+        assertEquals(500.0, account.getBalance(), 0.01);
+    }
+
+    @Test(expected = InsufficientFundsException.class)
+    public void testWithdrawInsufficientFunds() {
+        account.withdraw(1500.0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWithdrawNegativeAmount() {
+        account.withdraw(-500.0);
+    }
+}
